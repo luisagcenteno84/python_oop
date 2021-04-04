374e6fb8debe15de5a5528c1b22f97ba1435567f
luisagcenteno84 <luisagcenteno@gmail.com>
2021-04-03 23:12:11 -0700

    first push

--- test/test.py
+++ test/test.py
@@ -7,3 +7,11 @@ response = requests.get('http://google.com')
 
 response
 # %%
+import car
+my_car = car.Car("yellow","beetle","1967")
+
+print(f"My car is {my_car.color}")
+# %%
+
+print(f"Wheels: {my_car.wheels}")
+# %%
